2459e5a311d8d70078661d7d505cc61a0288e8a8
thilina22 <test@123gmail.com>
2023-01-07 23:58:03 +0530

    Added student regi form
    Added html,Css,appmodule ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RegStuComponent } from './reg-stu/reg-stu.component';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -16,7 +25,20 @@ import { RegStuComponent } from './reg-stu/reg-stu.component';
     BrowserModule,
     SharedModule,
     AppRoutingModule,
-    NoopAnimationsModule
+
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatSidenavModule
+    
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
