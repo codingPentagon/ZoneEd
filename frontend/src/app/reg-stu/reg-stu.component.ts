75f939f8fdcf4b1733d2c538f2adac19eaf472b7
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-01-23 00:46:21 +0530

    working on taking inputs from reg component to nav component

--- frontend/src/app/reg-stu/reg-stu.component.ts
+++ frontend/src/app/reg-stu/reg-stu.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./reg-stu.component.css']
 })
 export class RegStuComponent {
-
+  navLinks:any[] = [
+    {path:'/studentreg', value:'Student Registration', icon:'person_add'}
+  ];
 }
--- frontend/src/app/shared/nav/nav.component.ts
+++ frontend/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  @Input() links: any[] = [];
 }
