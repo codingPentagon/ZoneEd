1ef5e7900d3069da36dc46f3dc4dc0e83c30a4f2
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-01-28 21:38:59 +0530

    implemented schedule teacher view

--- frontend/src/app/schedule-tchr/schedule-tchr.component.ts
+++ frontend/src/app/schedule-tchr/schedule-tchr.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DataSource} from "@angular/cdk/collections";
 
 @Component({
   selector: 'app-schedule-tchr',
@@ -6,9 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./schedule-tchr.component.css']
 })
 export class ScheduleTchrComponent {
+
+  navLinks = [
+    {path:'', value:'Dashboard', icon:'space_dashboard'},
+    {path:'/scheduletchr', value:'Schedule', icon:'schedule'}
+  ];
   schedule = [
-    { day: 'Monday', mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A"},
+    { period: 1, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 2, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 3, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 4, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 5, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 6, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 7, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" },
+    { period: 8, mon:"10A", tue:"10A", wed:"10A", thu:"10A", fri:"10A" }
   ];
 
-  displayedColumns: string[] = ['day','mon','tue','wed','thu','fri'];
+  displayedColumns: string[] = ['period','mon','tue','wed','thu','fri'];
 }
