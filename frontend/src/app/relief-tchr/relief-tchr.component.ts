1dc22cdee89027dd466f42960048a64227260d1f
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-02-20 15:36:14 +0530

    implemented relief allocation card and bound with ts

--- frontend/src/app/relief-tchr/relief-tchr.component.ts
+++ frontend/src/app/relief-tchr/relief-tchr.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./relief-tchr.component.css']
 })
 export class ReliefTchrComponent {
-
+  reliefAllocation = [
+    {class:'10B',subject:'Science',fromTime:'8.50',toTime:'9.30',period:3,assmntCount:1},
+    {class:'10A',subject:'Sinhala',fromTime:'12.10',toTime:'11.30',period:4,assmntCount:2},
+    {class:'11B',subject:'English',fromTime:'12.50',toTime:'13.30',period:8,assmntCount:0},
+    {class:'9B',subject:'Maths',fromTime:'11.10',toTime:'11.50',period:5,assmntCount:1},
+    {class:'8B',subject:'ICT',fromTime:'12.10',toTime:'12.50',period:3,assmntCount:0}
+  ]
 }
