7726cab5ed255d44090d010063dbba105404e6b8
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-02-20 17:53:21 +0530

    implemented assessment card content

--- frontend/src/app/relief-tchr/relief-tchr.component.ts
+++ frontend/src/app/relief-tchr/relief-tchr.component.ts
@@ -14,5 +14,13 @@ export class ReliefTchrComponent {
     {class:'9B',subject:'Maths',fromTime:'11.10',toTime:'11.50',period:5,assmntCount:1},
     {class:'9B',subject:'Maths',fromTime:'11.10',toTime:'11.50',period:5,assmntCount:1},
     {class:'8B',subject:'ICT',fromTime:'12.10',toTime:'12.50',period:3,assmntCount:0}
+  ];
+
+  fwdAssessments = [
+    {fileName:"Assessment 2.pdf",class:"8A",time:"8.00"},
+    {fileName:"Assessment 3.pdf",class:"9A",time:"9.00"},
+    {fileName:"Assessment 1.docx",class:"8B",time:"8.30"},
+    {fileName:"Assessment 4.doc",class:"10A",time:"9.00"},
+    {fileName:"Assessment 2.pdf",class:"10B",time:"7.00"},
   ]
 }
