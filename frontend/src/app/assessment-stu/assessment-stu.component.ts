5219bb3dc452f3c89ab2e728096d9be1a2a7740a
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-02-21 12:05:03 +0530

    implemented nested object arrays and bound with selection triggers

--- frontend/src/app/assessment-stu/assessment-stu.component.ts
+++ frontend/src/app/assessment-stu/assessment-stu.component.ts
@@ -7,12 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AssessmentStuComponent {
 
-  subjects = ["Science","Sinhala","English","Maths","Buddhism","Music"];
-
-  assessments  = [
-    {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
-    {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
-    {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
-    {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
-  ]
+  selectedSubject = 0;
+  subjectAssessments = [
+    {subject:'9A',assessments:[
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {subject:'10A',assessments:[
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {subject:'11A',assessments:[
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {subject:'10B',assessments:[
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+  ];
 }
--- frontend/src/app/assessment-tchr/assessment-tchr.component.ts
+++ frontend/src/app/assessment-tchr/assessment-tchr.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AssessmentTchrComponent {
 
-  classes = ["8A","9A","10A","11A"];
+  selectedCls = 0;
+  clsAssessments = [
+    {class:'9A',assessments:[
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 1.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {class:'10A',assessments:[
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 2.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {class:'11A',assessments:[
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 3.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+    {class:'10B',assessments:[
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"},
+        {fileName:"Assessment 4.pdf",date:"01/02/2023",time:"8.30"}
+      ]
+    },
+  ];
+
 }
