b938ac4f8d23433e61025596e439717ab482098e
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-02-22 17:34:29 +0530

    implemented the functionalities and bound with ts

--- frontend/src/app/project-zonal/project-zonal.component.ts
+++ frontend/src/app/project-zonal/project-zonal.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class ProjectZonalComponent {
 
+
+  schoolProjsProps = [
+    {schoolname:'Poramadulla Centarl College',
+      proposals:[
+        {title:"Ruk Ropanaya",date:"01/02/2023",status:"Pending",feedback:null},
+        {title:"Science Camp",date:"01/02/2023",status:"Rejected",feedback:"Need improvements"},
+        {title:"English Day",date:"01/02/2023",status:"Accepted",feedback:"Good"},
+        {title:"Literature Camp",date:"01/02/2023",status:"Accepted",feedback:"Good"},
+      ],
+      projects:[
+        {name:'Ruk Ropanaya',
+          milestones:[
+            {title:"Collect Plants",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
+              proofs:[
+                {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
+                {fileName:"22.mp4",fileType:"movie",uploadedOn:"01/02/2023"}
+              ]
+            },
+            {title:"Ruk Ropanaya",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
+              proofs:[
+                {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
+                {fileName:"2.jpg",fileType:"movie",uploadedOn:"01/02/2023"}
+              ]
+            },
+          ]
+        },
+        {name:'Science Camp',
+          milestones:[
+            {title:"Collect Plants",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
+              proofs:[
+                {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
+                {fileName:"22.mp4",fileType:"movie",uploadedOn:"01/02/2023"}
+              ]
+            },
+          ]
+        },
+      ],
+    },
+    {schoolname:'Pushpadana Girls College',
+      proposals:[
+        {title:"Ruk Ropanaya",date:"01/02/2023",status:"Pending",feedback:null},
+        {title:"Science Camp",date:"01/02/2023",status:"Rejected",feedback:"Need improvements"},
+        {title:"English Day",date:"01/02/2023",status:"Accepted",feedback:"Good"},
+      ],
+      projects:[
+        {name:'Science Camp',
+          milestones:[
+            {title:"Ruk Ropanaya",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
+              proofs:[
+                {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
+                {fileName:"22.mp4",fileType:"movie",uploadedOn:"01/02/2023"}
+              ]
+            },
+            {title:"Ruk Ropanaya",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
+              proofs:[
+                {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
+                {fileName:"2.jpg",fileType:"movie",uploadedOn:"01/02/2023"}
+              ]
+            },
+          ]
+        },
+      ],
+    }
+  ];
+
+  selectedSchool = 0;
+  selectedProject = 0;
+
+  feedbackAdd=false;
+
+  feedbackAddToggle(){
+    this.feedbackAdd=!this.feedbackAdd;
+  }
 }
