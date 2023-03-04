9f45aeafc1d2a07b46ed174ff5fb2c74f903896a
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-04 11:10:42 +0530

    configured routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 
 import {NavComponent} from "./shared/nav/nav.component";
+import {LeavePrinComponent} from "./leave-prin/leave-prin.component";
+import {LeaveTchrComponent} from "./leave-tchr/leave-tchr.component";
 
 const routes: Routes = [
   { path: '', component: NavComponent },
+  { path: 'leaveprin', component: LeavePrinComponent },
+  { path: 'leavetchr', component: LeaveTchrComponent }
 ]
 
 @NgModule({
