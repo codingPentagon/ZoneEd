40f6e0aff1f2c375ca210ed0de394d65d4409673
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-11 20:33:54 +0530

    cleanup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-
-import {RegStuComponent} from "./reg-stu/reg-stu.component";
 import {NavComponent} from "./shared/nav/nav.component";
-import {HeaderComponent} from "./shared/header/header.component";
 
 const routes: Routes = [
-  {path: '', component: NavComponent},
-  {path: 'studentreg', component: RegStuComponent, children: [{path: 'page1', component: HeaderComponent}]},
-  {path: 'home', component: NavComponent, children: [{path: 'header', component: HeaderComponent}]}]
+  {path: '', component: NavComponent}
+]
 
 @NgModule({
   declarations: [],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegStuComponent } from './reg-stu/reg-stu.component';
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatTabsModule} from "@angular/material/tabs";
@@ -13,7 +11,6 @@ import {MatTabsModule} from "@angular/material/tabs";
 @NgModule({
   declarations: [
     AppComponent,
-    RegStuComponent,
   ],
   imports: [
     BrowserModule,
