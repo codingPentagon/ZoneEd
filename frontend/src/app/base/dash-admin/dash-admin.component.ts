5ca068b9a662c5a5873ebaf9a5f08d73d7dfe952
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-17 01:22:03 +0530

    created layout and added user id extraction

--- frontend/src/app/base/dash-admin/dash-admin.component.ts
+++ frontend/src/app/base/dash-admin/dash-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-dash-admin',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./dash-admin.component.css']
 })
 export class DashAdminComponent {
+  constructor(private route:ActivatedRoute) {
+  }
 
+  userID:number=this.route.snapshot.params['userID'];
 }
