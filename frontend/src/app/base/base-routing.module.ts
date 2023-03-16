bc66ce976498e0f70f90553011f05836c21e57dc
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-17 00:46:05 +0530

    added all routes

--- frontend/src/app/base/base-routing.module.ts
+++ frontend/src/app/base/base-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashTchrComponent} from "./dash-tchr/dash-tchr.component";
 import {DashStuComponent} from "./dash-stu/dash-stu.component";
+import {DashPrinComponent} from "./dash-prin/dash-prin.component";
+import {DashParentComponent} from "./dash-parent/dash-parent.component";
+import {DashZonalComponent} from "./dash-zonal/dash-zonal.component";
+import {DashAdminComponent} from "./dash-admin/dash-admin.component";
 
 const routes: Routes = [
   {path:'tchr/:userID', component:DashTchrComponent},
+  {path:'stu/:userID', component:DashStuComponent},
+  {path:'prin/:userID', component:DashPrinComponent},
+  {path:'parent/:userID', component:DashParentComponent},
+  {path:'zonal/:userID', component:DashZonalComponent},
+  {path:'admin/:userID', component:DashAdminComponent},
 ];
 
 @NgModule({
