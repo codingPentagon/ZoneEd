4f449946d21775e42d623d071930e10fbe1e7254
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-17 00:43:07 +0530

    added dynamic routing and extracted user id from path

--- frontend/src/app/base/base-routing.module.ts
+++ frontend/src/app/base/base-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashTchrComponent} from "./dash-tchr/dash-tchr.component";
+import {DashStuComponent} from "./dash-stu/dash-stu.component";
 
 const routes: Routes = [
-  {path:'', component:DashTchrComponent}
+  {path:'tchr/:userID', component:DashTchrComponent},
 ];
 
 @NgModule({
--- frontend/src/app/base/dash-tchr/dash-tchr.component.ts
+++ frontend/src/app/base/dash-tchr/dash-tchr.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-dash-tchr',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class DashTchrComponent {
 
+  constructor(private route:ActivatedRoute) {
+  }
+  userID:number = this.route.snapshot.params['userID'];
+
+  ngOnInit() {
+    console.log(this.userID)
+  }
 }
