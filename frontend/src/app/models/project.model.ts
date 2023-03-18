b72840884fe67685c425e9baa005ef75a5afd51d
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-03-18 18:47:12 +0530

    created project and proposal models

--- frontend/src/app/models/project.model.ts
+++ frontend/src/app/models/project.model.ts
@@ -1,2 +1,27 @@
 export interface Project {
+  id:number;
+  createdDate:Date;
+  title:String;
+  proposalID:number;
+  startDate:Date;
+  endDate:Date;
+  responsiblePersonID:number;
+  schoolID:number;
+  principalID:number;
+  milestones:Milestone[];
+}
+
+interface Milestone {
+  feedback:String;
+  dueDate:Date;
+  createdDate:Date;
+  title:String;
+  proofs:Proof[];
+}
+
+interface Proof {
+  filePath:String;
+  fileName:String;
+  file:File;
+
 }
--- frontend/src/app/models/proposal.model.ts
+++ frontend/src/app/models/proposal.model.ts
@@ -1,2 +1,18 @@
 export interface Proposal {
+  id:number;
+  title:String;
+  status:String;
+  createdDate:Date;
+  comment:String;
+  feedback:String;
+  fileName:String;
+  schoolID:number;
+  pricipalID:number;
+  documents:Document[];
+}
+
+interface Document {
+  filePath:String;
+  fileName:String;
+  file:File;
 }
