d6c1b3fb5b8229a73aad152c05235eb5160ae55f
TharinduDananjaya <115687439+TharinduDananjaya@users.noreply.github.com>
2023-03-22 21:37:41 +0530

    Implemented new notification module for firebase cloud services.

--- frontend/src/app/services/notification.service.ts
+++ frontend/src/app/services/notification.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Notification} from "../models/notification.model";
 import {HttpClient} from "@angular/common/http";
-import {AngularFireMessaging} from '@angular/fire/compat/messaging'
-import {} from "@angular/fire/compat/messaging";
+import {getMessaging,getToken,onMessage} from '@angular/fire/messaging'
+import { BehaviorSubject } from 'rxjs'
 
 const url = 'http://localhost:8080/notifications/'
 
@@ -11,13 +11,38 @@ const url = 'http://localhost:8080/notifications/'
   providedIn: 'root'
 })
 export class NotificationService {
+  message :any=null;
 
-  constructor(private http: HttpClient, private fireNotification:AngularFireMessaging) {
-    this.fireNotification.requestToken.subscribe((token)=>{
-      console.log(token);
-    });
+  constructor(private http: HttpClient) {
 
   }
+  ngOnInit(){
+    this.requestPermission();
+    this.listen();
+  }
+
+  private requestPermission() {
+    const messaging = getMessaging();
+    getToken(messaging,{vapidKey:'BP5k-BYYIzmnZZvCbwWiddeELjekqDkuV8x2OVYS1c_sBVF3jhzaHDIkN4WRrUv1infhRDiFZ9w0rR066nnSP7M'}).then(
+      (currentToken)=>{
+        if (currentToken){
+          console.log("Got token");
+        }
+        else {
+          console.log("No Token");
+        }
+      }
+    );
+  }
+
+  private listen() {
+    const messaging = getMessaging();
+    onMessage(messaging,(payLoad)=>{
+      this.message=payLoad;
+      console.log(payLoad);
+    })
+  }
+
 
   createNotification(receiverID:number, content:string, event:string){
     const notif :Notification = {
@@ -40,6 +65,7 @@ export class NotificationService {
   updateNotification(notification:Notification){
     return this.http.put(url,notification);
   }
+
 
 }
 
--- frontend/src/firebase-messaging-sw.js
+++ frontend/src/firebase-messaging-sw.js
@@ -1,1 +1,7 @@
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging-compat.js');
+
+
+const messaging = firebase.messaging();
+
 
