49f225e7a47c0a0f22b82b1af5d30e1f9ec44d99
thilina22 <test@123gmail.com>
2023-03-23 18:28:35 +0530

    updated stu controller in backend

--- backend/src/main/java/codingpentagon/sms/backend/RoleController/StuRegController.java
+++ backend/src/main/java/codingpentagon/sms/backend/RoleController/StuRegController.java
@@ -4,7 +4,7 @@ package codingpentagon.sms.backend.RoleController;
 import RoleModel.StudentReg;
 import codingpentagon.sms.backend.RoleRepo.StuRegRepo;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/stu")
+//@RequestMapping("api/v1/stu")
 @CrossOrigin (origins = "http://localhost:4200")
 public class StuRegController {
 
     @Autowired
-    StuRegRepo stuRegRepo;
+    private StuRegRepo stuRegRepo;
 
     @PostMapping("/regStu")
     public StudentReg register(@RequestBody StudentReg studentReg){
 
         return  stuRegRepo.save(studentReg);
 
+    }
+    @GetMapping("/regStuData")
+    public List<StudentReg> getStudent (){
+        return stuRegRepo.findAll();
     }
 
     @GetMapping("/regStuData/{fullName}")
