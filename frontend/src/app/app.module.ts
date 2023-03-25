269bb741b41f30169aa008eb4ea0e9756817aa5f
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-25 21:16:05 +0530

    experimented with navy and mediumspringgreen

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,21 +9,23 @@ import { RegStuComponent } from './reg-stu/reg-stu.component';
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatTabsModule} from "@angular/material/tabs";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     RegStuComponent,
   ],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatTabsModule
-  ],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatCardModule,
+        MatTabsModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
