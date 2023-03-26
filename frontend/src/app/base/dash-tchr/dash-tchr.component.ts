bb7fcdac75889874527843f52a0a061d0f6a4c8e
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-03-27 02:11:13 +0530

    created nav links and bound with nav bar attribute

--- frontend/src/app/base/dash-tchr/dash-tchr.component.ts
+++ frontend/src/app/base/dash-tchr/dash-tchr.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {NavLinkSet} from "../../models/nav-link.model";
 
 @Component({
   selector: 'app-dash-tchr',
@@ -7,6 +8,37 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./dash-tchr.component.css']
 })
 export class DashTchrComponent {
+
+  navLinks:NavLinkSet[]=[
+    {parentLink:{value:'Dashboard',icon:'space_dashboard',path:'/teacher'},
+      childLinks:null
+    },
+    {parentLink:{value:'Class Management',icon:'class',path:'class_management/students'},
+      childLinks:[
+        {value:'Student Registration',icon:'person_add',path:'class_management/students'},
+        {value:'Student Attendance',icon:'rule',path:'class_management/attendance'},
+        {value:'Student Grades',icon:'text_increase',path:'class_management/grades'}
+      ]
+    },
+    {parentLink:{value:'Subject Management',icon:'menu_book',path:'project_management/assessments'},
+      childLinks:[
+        {value:'Assessments',icon:'assignment',path:'project_management/assessments'},
+        {value:'Relief Allocations',icon:'supervisor_account',path:'project_management/relief'}
+      ]
+    },
+    {parentLink:{value:'Leave Management',icon:'work_history',path:'school_calendars'},
+      childLinks:null
+    },
+    {parentLink:{value:'My Profile',icon:'account_circle',path:'my_profile'},
+      childLinks:null
+    },
+    {parentLink:{value:'Mails',icon:'mail',path:'mails'},
+      childLinks:null
+    },
+    {parentLink:{value:'Reports',icon:'assessment',path:'reports'},
+      childLinks:null
+    }
+  ];
 
   constructor(private route:ActivatedRoute) {
   }
