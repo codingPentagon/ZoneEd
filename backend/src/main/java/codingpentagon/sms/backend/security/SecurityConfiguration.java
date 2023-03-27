7b05f56604be43ceb4e9eed5ea824a62e2a0cbbe
thilina22 <test@123gmail.com>
2023-03-28 02:16:44 +0530

    Comment securit config class and defendency

--- backend/src/main/java/codingpentagon/sms/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/codingpentagon/sms/backend/security/SecurityConfiguration.java
@@ -1,43 +1,43 @@
-package codingpentagon.sms.backend.security;
+// package codingpentagon.sms.backend.security;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@Configuration
-@EnableWebSecurity
+// @Configuration
+// @EnableWebSecurity
 
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      auth.inMemoryAuthentication()
-      .withUser("admin").password(passwordEncoder().encode("123")).roles("admin")
-      .and()
-      .withUser("user2").password(passwordEncoder().encode("dhdh1")).roles("admin");
+// public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//     @Override
+//     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//       auth.inMemoryAuthentication()
+//       .withUser("admin").password(passwordEncoder().encode("123")).roles("admin")
+//       .and()
+//       .withUser("user2").password(passwordEncoder().encode("dhdh1")).roles("admin");
       
       
       
-    }
+//     }
 
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-        .anyRequest().permitAll()
-        .and()
-        .httpBasic();
-    }
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception {
+//         http.authorizeRequests()
+//         .anyRequest().permitAll()
+//         .and()
+//         .httpBasic();
+//     }
 
-    @Bean
-PasswordEncoder passwordEncoder(){
-    return new BCryptPasswordEncoder();
-}
-}
+//     @Bean
+// PasswordEncoder passwordEncoder(){
+//     return new BCryptPasswordEncoder();
+// }
+// }
