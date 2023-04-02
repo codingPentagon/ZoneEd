ebab2ba964b124069dcd67211959e8cab86ffd6c
TharinduDananjaya <115687439+TharinduDananjaya@users.noreply.github.com>
2023-04-02 16:07:41 +0530

    Removed firebase config from service worker.

--- frontend/src/firebase-messaging-sw.js
+++ frontend/src/firebase-messaging-sw.js
@@ -1,17 +1,3 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-const fire=firebase.initializeApp({
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
-});
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = fire.messaging();
