5b4f40899134b5f18d30b13795e68b135728d8b3
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-13 18:20:17 +0530

    created services module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatChipsModule} from "@angular/material/chips";
 import { ReliefPrinComponent } from './relief-prin/relief-prin.component';
 import { ReliefTchrComponent } from './relief-tchr/relief-tchr.component';
 import {FormsModule} from "@angular/forms";
+import {ServicesModule} from "./services/services.module";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import {FormsModule} from "@angular/forms";
     MatSelectModule,
     MatButtonModule,
     MatChipsModule,
-    FormsModule
+    FormsModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
