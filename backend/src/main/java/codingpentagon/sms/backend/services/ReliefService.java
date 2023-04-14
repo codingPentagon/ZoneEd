ceb5dc1eaa953835e9d5b03c24bf5d59bca0b4b6
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-14 13:39:47 +0530

    set the time to 5:30 instead of setting the time zone

--- backend/src/main/java/codingpentagon/sms/backend/services/ReliefService.java
+++ backend/src/main/java/codingpentagon/sms/backend/services/ReliefService.java
@@ -21,15 +21,15 @@ public class ReliefService {
 
     public List<Teacher> findTeachersOnLeave(int sclID) {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR, 0);
-        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR, 5);
+        calendar.set(Calendar.MINUTE, 30);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        calendar.set(Calendar.AM_PM, Calendar.AM);
-        calendar.set(Calendar.ZONE_OFFSET, 0);
+        calendar.set(Calendar.AM_PM, Calendar.AM);      //equals to 0:00 in UTC
 
         List<Integer> teacherIDs = this.leaveRecordRepository.findBySclIDAndDatesContaining(sclID, calendar.getTime()).stream().map(LeaveRecord::getTeacherID).toList();
 
         return this.teacherRepository.findAllById(teacherIDs);
     }
+
 }
