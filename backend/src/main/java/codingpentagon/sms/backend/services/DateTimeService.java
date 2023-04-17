59eb8a5462a040cfbf021abea59529e41aea8fea
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-18 01:25:43 +0530

    created service get Calendar class generated date time

---
+++ backend/src/main/java/codingpentagon/sms/backend/services/DateTimeService.java
@@ -0,0 +1,29 @@
+package codingpentagon.sms.backend.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service
+public class DateTimeService {
+    public Calendar getToday() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR, 5);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.AM_PM, Calendar.AM);      //equals to 0:00 in UTC
+        calendar.set(Calendar.DATE, 17);
+        return calendar;
+    }
+
+    public Calendar getTomorrow() {
+        Calendar calendar = this.getToday();
+        calendar.add(Calendar.DATE, 1);
+        return calendar;
+    }
+
+    public String getTodayName() {
+        return this.getToday().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, java.util.Locale.ENGLISH);
+    }
+}
