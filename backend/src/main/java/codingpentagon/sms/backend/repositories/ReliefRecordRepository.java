aa7a56f5ab00a5bdd359993ecae69b58548c31ae
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-18 02:31:22 +0530

    integrated date time service, implemented functionality to filter out already allocated teachers for each period

--- backend/src/main/java/codingpentagon/sms/backend/repositories/ReliefRecordRepository.java
+++ backend/src/main/java/codingpentagon/sms/backend/repositories/ReliefRecordRepository.java
@@ -4,7 +4,9 @@ import codingpentagon.sms.backend.models.ReliefRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Date;
+import java.util.List;
 
 public interface ReliefRecordRepository extends MongoRepository<ReliefRecord, Integer> {
     ReliefRecord findBySclIDAndPeriodAndClassNameAndDateBetween(int sclID, int period, String className, Date date1, Date date2);
+    List<ReliefRecord> findBySclIDAndPeriodAndDateBetween(int sclID, int period, Date date1, Date date2);
 }
--- backend/src/main/java/codingpentagon/sms/backend/services/ReliefService.java
+++ backend/src/main/java/codingpentagon/sms/backend/services/ReliefService.java
@@ -15,43 +15,50 @@ public class ReliefService {
     private final TeacherRepository teacherRepository;
     private final ScheduleRepository scheduleRepository;
     private final ReliefRecordRepository reliefRecordRepository;
+    private final DateTimeService dateTimeService;
 
     @Autowired
-    public ReliefService(LeaveService leaveService, TeacherRepository teacherRepository, ScheduleRepository scheduleRepository, ReliefRecordRepository reliefRecordRepository) {
+    public ReliefService(LeaveService leaveService, TeacherRepository teacherRepository, ScheduleRepository scheduleRepository, ReliefRecordRepository reliefRecordRepository, DateTimeService dateTimeService) {
         this.leaveService = leaveService;
         this.teacherRepository = teacherRepository;
         this.scheduleRepository = scheduleRepository;
         this.reliefRecordRepository = reliefRecordRepository;
+        this.dateTimeService = dateTimeService;
     }
 
     public List<ReliefSlotCandidates> findReliefSlotsCandidates(int sclID, int teacherID) {
         List<ReliefSlotCandidates> reliefSlotsWithCandidates = new ArrayList<>();
-        Calendar calendar = Calendar.getInstance();
-        String dayName = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH).toLowerCase().substring(0, 3);
+        String dayName = this.dateTimeService.getTodayName().toLowerCase().substring(0, 3);
 
         //getting vacant classes with periods
         Schedule schedule1 = this.scheduleRepository.findByTeacherID(teacherID);
         schedule1.getSchedule().forEach((SchedulePeriod schedulePeriod) -> {
             String slot = schedulePeriod.getSlotByDay().get(dayName);
+            int period = schedulePeriod.getPeriod();
             if (!slot.equals("---")) {
                 ReliefSlotCandidates reliefSlotCandidates = new ReliefSlotCandidates();
                 reliefSlotCandidates.setPeriod(schedulePeriod.getPeriod());
                 reliefSlotCandidates.setClassName(slot);
-                //finding if any allocated teacher for the vacant
-                reliefSlotCandidates.setAllocatedTeacherID(this.findAllocatedTeacher(sclID, schedulePeriod.getPeriod(), slot));
+                //finding if any teacher allocated for the vacant
+                int allocatedTeacherID = this.findAllocatedTeacher(sclID, period, slot);
+                reliefSlotCandidates.setAllocatedTeacherID(allocatedTeacherID);
                 //finding available teachers for the vacant
-                reliefSlotCandidates.setAvailableTeachers(this.findAvailableTeachers(dayName, schedulePeriod.getPeriod(), sclID));
+                reliefSlotCandidates.setAvailableTeachers(this.findAvailableTeachers(sclID ,dayName, period, allocatedTeacherID));
                 reliefSlotsWithCandidates.add(reliefSlotCandidates);
             }
         });
         return reliefSlotsWithCandidates;
     }
 
-    private List<Teacher> findAvailableTeachers(String dayName, int period, int sclID) {
+    private List<Teacher> findAvailableTeachers(int sclID, String dayName, int period, int allocatedTeacherID) {
         List<Teacher> availableTeachers = new ArrayList<>();
+        List<Integer> unavailableTeacherIDs = new ArrayList<>();
 
-        List<Integer> teachersOnLeaveIDs = this.leaveService.findLeavesToday(sclID).stream().map(Teacher::getId).toList();
-        List<Teacher> teachersPresent = this.teacherRepository.findBySclIDAndIdNotIn(sclID, teachersOnLeaveIDs);
+        unavailableTeacherIDs.addAll(this.leaveService.findLeavesToday(sclID).stream().map(Teacher::getId).toList());
+        //adds allocated teachers as unavailable for the period including the teacher who is already allocated
+        unavailableTeacherIDs.addAll(findAllocatedTeacherIDs(sclID, period));
+
+        List<Teacher> teachersPresent = this.teacherRepository.findBySclIDAndIdNotIn(sclID, unavailableTeacherIDs);
 
         for (Teacher teacher : teachersPresent) {
             Schedule schedule = this.scheduleRepository.findByTeacherID(teacher.getId());
@@ -65,6 +72,10 @@ public class ReliefService {
                 }
             });
         }
+        //gets already allocated teacher for this vacant and adds to the list
+        if (allocatedTeacherID != 0) {
+            this.teacherRepository.findById(allocatedTeacherID).ifPresent(availableTeachers::add);
+        }
         return availableTeachers;
     }
 
@@ -76,20 +87,29 @@ public class ReliefService {
     }
 
     public int findAllocatedTeacher(int sclID, int period, String className) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 5);
-        calendar.set(Calendar.MINUTE, 30);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        calendar.set(Calendar.AM_PM, Calendar.AM);
-        Date date1 = calendar.getTime();
-        calendar.add(Calendar.DATE, 1);
-        Date date2 = calendar.getTime();
+        Date date1 = this.dateTimeService.getToday().getTime();
+        Date date2 = this.dateTimeService.getTomorrow().getTime();
 
         try {
             return this.reliefRecordRepository.findBySclIDAndPeriodAndClassNameAndDateBetween(sclID, period, className, date1, date2).getAllocatedTeacherID();
         } catch (NullPointerException e) {
             return  0;
+        }
+    }
+
+    public List<Integer> findAllocatedTeacherIDs(int sclID, int period) {
+        List<Integer> allocatedTeacherIDs = new ArrayList<>();
+        Date date1 = this.dateTimeService.getToday().getTime();
+        Date date2 = this.dateTimeService.getTomorrow().getTime();
+
+        try {
+            this.reliefRecordRepository.findBySclIDAndPeriodAndDateBetween(sclID, period, date1, date2).forEach(reliefRecord -> {
+                allocatedTeacherIDs.add(reliefRecord.getAllocatedTeacherID());
+            });
+            return allocatedTeacherIDs;
+        }
+        catch (NullPointerException e) {
+            return null;
         }
     }
 }
