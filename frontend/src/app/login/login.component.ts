15a95b4648b89c784f176a1819945aca93d75f2c
thilina22 <test@123gmail.com>
2023-04-26 20:58:14 +0530

    added alert for login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit {
           this.router.navigate([''])
         }else
         {
-          alert("Login Unsuccessfully")
+          alert("Login Unsuccessfully");
         }
 
       
