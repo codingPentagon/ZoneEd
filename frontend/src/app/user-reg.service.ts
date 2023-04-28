a4158121f78cc19867b0ce2442cd02c712e916f7
thilina22 <test@123gmail.com>
2023-04-28 09:52:47 +0530

    added comment

--- frontend/src/app/user-reg.service.ts
+++ frontend/src/app/user-reg.service.ts
@@ -18,17 +18,17 @@ export class UserRegService {
   fullName: any;
   
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }//injet http client module for hppt request purposes
 
 
-  sendStuData(stu:Stu) {
+  sendStuData(stu:Stu) {//when reg student this method will invoke and send post request to backend
     console.log(stu);
-    return this.httpClient.post("http://localhost:8080/api/auth/reg/stu",stu);
+    return this.httpClient.post("http://localhost:8080/api/auth/reg/stu",stu); //rest api for reg student
   }
   
-  sendTchData(tch:Tchr) {
+  sendTchData(tch:Tchr) {//when reg teacher this method will invoke and send post request to backend
     console.log(tch);
-    return this.httpClient.post("http://localhost:8080/api/auth/reg/tch",tch);
+    return this.httpClient.post("http://localhost:8080/api/auth/reg/tch",tch);//rest api for reg teacher
   }
 
 
