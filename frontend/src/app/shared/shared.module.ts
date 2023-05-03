2f5821550595b768ed2258a69cfc72fad810d770
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-05-04 01:30:01 +0530

    added cards for drawer items and set layout

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import {CalendarComponent} from "./calendar/calendar.component";
 import {CalendarToggleService} from "./calendar/calendar-toggle.service";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxEventCalendarModule} from "ngx-event-calendar";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
@@ -33,6 +34,7 @@ import {NgxEventCalendarModule} from "ngx-event-calendar";
         MatToolbarModule,
         HttpClientModule,
         NgxEventCalendarModule,
+        MatCardModule,
 
     ],
 
