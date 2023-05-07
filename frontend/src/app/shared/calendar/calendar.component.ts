d454d2b8ee98bd4bc4cc6f9a2c716b49e093979c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-05-07 23:23:05 +0530

    created functionalities to check holiday and event dates from holidays and events arrays

--- frontend/src/app/shared/calendar/calendar.component.ts
+++ frontend/src/app/shared/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {CalendarToggleService} from "./calendar-toggle.service";
 import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
+import {SchoolHoliday,SchoolEvent} from "../../models/calendar.model";
 
 @Component({
   selector: 'app-calendar',
@@ -12,6 +13,25 @@ export class CalendarComponent {
   //calendar toggle
   isOpen:boolean = false;
   selected:Date|null = new Date();
+  sclID:number = 5555;
+  holidays:SchoolHoliday[] = [
+    {id:1,title:"test holi 1",date:new Date(2023,4,10)},
+    {id:2,title:"test holi 2",date:new Date(2023,4,15)},
+    {id:3,title:"test holi 3",date:new Date(2023,4,20)},
+  ];
+  events:SchoolEvent[] = [
+    {id:1,title:"test event 1",sclID:5555,dates:[
+        new Date(2023,4,1),
+        new Date(2023,4,2),
+      ]
+    },
+    {id:2,title:"test event 2",sclID:5555,dates:[
+        new Date(2023,4,25),
+        new Date(2023,4,26),
+        new Date(2023,4,27),
+      ]
+    },
+  ];
 
   constructor(private calToggleService:CalendarToggleService) {
   }
@@ -19,15 +39,28 @@ export class CalendarComponent {
   ngOnInit() {
     this.calToggleService.change.subscribe(isOpen => {
       this.isOpen = isOpen;
-      console.log(new Date().toLocaleDateString());
     });
   }
 
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate) => {
-    const holiday = new Date(2023,4,10).toLocaleDateString();
-    const event = new Date(2023,4,15).toLocaleDateString();
-    if (cellDate.toLocaleDateString()==holiday) {return 'holiday'}
-    if (cellDate.toLocaleDateString()==event) {return 'event'}
+    let holidayCheck = this.holidays.some(holiday => {
+      return holiday.date.toLocaleDateString() == cellDate.toLocaleDateString();
+    })
+
+    if (holidayCheck) {
+      return 'holiday'
+    }
+
+    let eventCheck:boolean = false;
+    for (const event of this.events) {
+      eventCheck = event.dates.some(date => {
+        return date.toLocaleDateString() == cellDate.toLocaleDateString();
+      });
+      if (eventCheck) {
+        return 'event';
+      }
+    }
+
     return '';
   };
 }
