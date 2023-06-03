5c9feba6b7fb562bc52f24cfbcfe5204f1cb045d
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-06-04 02:33:01 +0530

    made subject mark a public class

--- backend/src/main/java/codingpentagon/sms/backend/models/Marksheet.java
+++ backend/src/main/java/codingpentagon/sms/backend/models/Marksheet.java
@@ -88,22 +88,3 @@ public class Marksheet {
     }
 }
 
-class SubjectMark{
-    private int subjectID;
-    private Integer mark;
-    public int getSubjectID() {
-        return subjectID;
-    }
-
-    public void setSubjectID(int subjectID) {
-        this.subjectID = subjectID;
-    }
-
-    public Integer getMark() {
-        return mark;
-    }
-
-    public void setMark(Integer mark) {
-        this.mark = mark;
-    }
-}
