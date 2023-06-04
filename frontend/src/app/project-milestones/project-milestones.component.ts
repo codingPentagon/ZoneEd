2890c87d3856e5b286097da29b23f8f74ead46a4
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-06-04 19:51:09 +0530

    implemented functionalities to create milestones

--- frontend/src/app/project-milestones/project-milestones.component.ts
+++ frontend/src/app/project-milestones/project-milestones.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ProjectService} from "../services/project.service";
+import {Project} from "../models/project.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-project-milestones',
@@ -8,29 +10,35 @@ import {ProjectService} from "../services/project.service";
 })
 export class ProjectMilestonesComponent {
 
-  sclID:number=555;
+  @Input() project!:Project;
+  milestoneAdd:boolean = false;
+  proofAdd:boolean = false;
 
-  constructor(private projectService:ProjectService) {
+  constructor(private projectService: ProjectService) {
   }
 
-  milestones=[
-    {title:"Ruk Ropanaya",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
-      proofs:[
-        {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"}
-      ]
-    },
-    {title:"Ruk Ropanaya",dueDate:"01/02/2023",feedback:"Good",status:"Approved",
-      proofs:[
-        {fileName:"1.jpg",fileType:"image",uploadedOn:"01/02/2023"},
-        {fileName:"2.jpg",fileType:"movie",uploadedOn:"01/02/2023"}
-      ]
-    },
-  ];
-
-  milestoneAdd:boolean = false;
 
   milestoneAddToggle(){
     this.milestoneAdd = !this.milestoneAdd;
   }
 
+  proofAddToggle(){
+    this.proofAdd = !this.proofAdd;
+  }
+
+  createMilestone(newMilestone: NgForm) {
+    this.project.milestones.push({
+      title: newMilestone.value.title,
+      createdDate: new Date(),
+      dueDate: new Date(newMilestone.value.dueDate),
+      feedback: "",
+      proofs: []
+    });
+    console.log(this.project)
+    this.projectService.addProject(this.project).subscribe({
+      complete: () => {
+        this.milestoneAddToggle();
+      }
+    });
+  }
 }
