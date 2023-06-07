fc6f9630e44a98ccecfbb00462eb7fe4e683c45e
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-06-08 00:40:34 +0530

    rename calendar side panel to calendar-drawer

--- frontend/src/app/models/calendar.model.ts
+++ frontend/src/app/models/calendar.model.ts
@@ -1,12 +1,21 @@
-export interface SchoolHoliday{
+export interface CalendarDetail{
+  id:number;
+  year:number;
+  name:string;
+  sclID:number;
+  status:string;
+}
+
+export interface Holiday{
   id:number;
   date:Date;
   title:string;
+  calendarID:number
 }
 
 export interface SchoolEvent{
   id:number;
   dates:Date[];
   title:string;
-  sclID:number
+  calendarID:number
 }
