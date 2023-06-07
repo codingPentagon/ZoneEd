d88499110845705f2fa628eb2d79e86010088352
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-06-07 17:24:31 +0530

    background notifications are working

--- frontend/src/firebase-messaging-sw.js
+++ frontend/src/firebase-messaging-sw.js
@@ -1,3 +1,13 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDNYmNI8dvHyNRpcp2JNSRdYCrUSz2vEyI",
+  authDomain: "zoneed-a61da.firebaseapp.com",
+  projectId: "zoneed-a61da",
+  storageBucket: "zoneed-a61da.appspot.com",
+  messagingSenderId: "896598316718",
+  appId: "1:896598316718:web:274c66582a3baa5aeabf3f"
+});
+
+const messaging = firebase.messaging();
