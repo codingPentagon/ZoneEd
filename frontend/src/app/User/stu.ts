21cc3e7272ea875bf0f04d042b048f0d7889068d
thilina22 <test@123gmail.com>
2023-06-11 17:22:17 +0530

    updated

--- frontend/src/app/User/stu.ts
+++ frontend/src/app/User/stu.ts
@@ -1,10 +1,10 @@
 export class Stu {
-    fullName!:String;
+    fullName = 'Thilina Nuwan Karunarathne'
     initName!:String;
-    address1!:String;
-    address2!:String;
-    gender!:String;
-    indexNo!:String;
-    email!:String;
+    address1='317/6 Bogahapitiya';
+    address2='Werellagama';
+    gender='male';
+    indexNo='204095D';
+    email='test@gmail.com';
     nic!:String;
 }
