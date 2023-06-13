9c4dca1df194cc1e36ebba3f64b0c552f475140f
thilina22 <test@123gmail.com>
2023-06-13 08:03:27 +0530

    updated

---
+++ backend/src/main/java/RoleModel/dto/response/StudentRegDto.java
@@ -0,0 +1,82 @@
+package RoleModel.dto.response;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import RoleModel.StudentReg;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentRegDto {
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    private String initName;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String address1;
+    private String address2;
+    @NotBlank
+    private String indexNo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+
+    @NotBlank
+    private String gender;
+
+    @NotBlank
+    private String stuClass;
+    private String admDate;
+    private String fatherName1;
+    private String faddress1;
+    private String faddress2;
+
+    @NotBlank
+    private String faNic;
+    private String faOccupation;
+    private String faContNo;
+
+    public StudentReg toStudentReg(StudentRegDto dto, ObjectMapper mapper) {// Java objects to JSON and vice versa
+        return mapper.convertValue(dto, StudentReg.class);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"fullName\":\"" + fullName + "\"," +
+                "\"initName\":\"" + initName + "\"," +
+                "\"email\":\"" + email + "\"," +
+                "\"password\":\"" + "***********" + "\"," +
+                "\"address1\":\"" + address1 + "\"," +
+                "\"address2\":\"" + address2 + "\"," +
+                "\"indexNo\":\"" + indexNo + "\"," +
+                "\"dob\":" + dob + "," +
+                "\"gender\":\"" + gender + "\"," +
+                "\"stuClass\":\"" + stuClass + "\"," +
+                "\"admDate\":\"" + admDate + "\"," +
+                "\"fatherName1\":\"" + fatherName1 + "\"," +
+                "\"faddress1\":\"" + faddress1 + "\"," +
+                "\"faddress2\":\"" + faddress2 + "\"," +
+                "\"faNic\":\"" + faNic + "\"," +
+                "\"faOccupation\":\"" + faOccupation + "\"," +
+                "\"faContNo\":\"" + faContNo + "\"" +
+                "}";
+    }
+    
+}
---
+++ backend/src/main/java/codingpentagon/sms/backend/services/AuthUserDetails.java
@@ -0,0 +1,88 @@
+package codingpentagon.sms.backend.services;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import RoleModel.AuthUser;
+
+public class AuthUserDetails implements UserDetails {
+    
+    private static final long serialVersionUID = 1L;
+
+    private String id;  // The ID of the user.
+
+    private String username;   // The username of the user.
+
+    private String email;  // The email of the user.
+
+    @JsonIgnore   //property should be ignored during the JSON serialization
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public AuthUserDetails(String id, String username, String email, String password,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }  // Constructor to initialize the AuthUserDetails object with the provided information.
+
+    public static AuthUserDetails build(AuthUser user) {
+        List<GrantedAuthority> authorities = user.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().toUpperCase()))
+                .collect(Collectors.toList());
+
+        return new AuthUserDetails( //parse the user data and authorities with user role
+                user.getId(),
+                user.getEmail(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+      return authorities;//This method returns the authorities (roles) assigned to the user.
+    }
+
+    @Override
+    public String getPassword() {
+        return password;//This method returns the user's password
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {//always returns true, indicating that the account is not locked.
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
